6db7a642ab1f25392d00ef1d432f2034cfafe5f1
Jason Paterson <jsn@juxt.pro>
2020-08-22 22:03:43 +0100

    Move term dotfiles to single dir

---
+++ dot/term/.config/fish/config.fish
@@ -0,0 +1,18 @@
+# fish_vi_key_bindings
+fzf_key_bindings
+
+set -gx fish_greeting ""
+
+# fzf
+set -gx FZF_DEFAULT_COMMAND "fd --type f"
+set -gx FZF_CTRL_T_COMMAND "fd"
+set -gx FZF_ALT_C_COMMAND "fd --type d"
+set -gx FZF_ALT_C_OPTS "--preview 'tree -C {} | head -200'"
+set -gx FZF_CTRL_T_OPTS "--preview 'bat --line-range :200 {} 2> /dev/null|| tree -C {} | head -200'"
+
+# neovim
+if type -q nvim
+  set -gx EDITOR nvim
+  set -gx VISUAL nvim
+  set -gx MANPAGER "nvim +Man!"
+end
