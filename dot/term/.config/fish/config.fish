eb001530243145d64690de5fc876c9563bd2f369
Jason Paterson <jsn@juxt.pro>
2020-08-23 23:44:30 +0100

    Add dircolors

--- dot/term/.config/fish/config.fish
+++ dot/term/.config/fish/config.fish
@@ -10,6 +10,8 @@ set -gx FZF_ALT_C_COMMAND "fd --type d"
 set -gx FZF_ALT_C_OPTS "--preview 'tree -C {} | head -200'"
 set -gx FZF_CTRL_T_OPTS "--preview 'bat --line-range :200 {} 2> /dev/null|| tree -C {} | head -200'"
 
+eval (dircolors -c)
+
 # neovim
 if type -q nvim
   set -gx EDITOR nvim
