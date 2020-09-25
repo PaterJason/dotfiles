4ac19c38c5f2897017d77f3ab687ab246190fbe1
Jason Paterson <jsn@juxt.pro>
2020-09-26 00:50:41 +0100

    I for one welcome our new gnome overlords

--- dot/term/.config/fish/config.fish
+++ dot/term/.config/fish/config.fish
@@ -2,11 +2,21 @@
 if type -q fzf
   fzf_key_bindings
 end
-set -gx FZF_DEFAULT_COMMAND "fd --type f"
-set -gx FZF_CTRL_T_COMMAND "fd"
-set -gx FZF_ALT_C_COMMAND "fd --type d"
-set -gx FZF_ALT_C_OPTS "--preview 'tree -C {} | head -200'"
-set -gx FZF_CTRL_T_OPTS "--preview 'bat --line-range :200 {} 2> /dev/null|| tree -C {} | head -200'"
+if type -q fd
+  set -gx FZF_DEFAULT_COMMAND "fd --type f"
+  set -gx FZF_CTRL_T_COMMAND "fd"
+  set -gx FZF_ALT_C_COMMAND "fd --type d"
+end
+if type -q tree
+  set -gx FZF_ALT_C_OPTS "--preview 'tree -C {} | head -200'"
+end
+if type -q bat && type -q tree
+  set -gx FZF_CTRL_T_OPTS "--preview 'bat --line-range :200 {} 2> /dev/null|| tree -C {} | head -200'"
+end
+
+if type -q bat
+  set -gx MANPAGER "sh -c 'col -bx | bat -l man -p'"
+end
 
 eval (dircolors -c)
 
@@ -14,7 +24,7 @@ eval (dircolors -c)
 if type -q nvim
   set -gx EDITOR nvim
   set -gx VISUAL nvim
-  set -gx MANPAGER "nvim +Man!"
+  # set -gx MANPAGER "nvim +Man!"
 end
 
 if test -d ~/bin
---
+++ sh/install_proton_ge.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+
+REPO=GloriousEggroll/proton-ge-custom
+VER=$(curl -s https://api.github.com/repos/${REPO}/releases/latest | grep -Po '"tag_name": "\K.*?(?=")')
+FILE=Proton-${VER}.tar.gz
+DST_DIR=${HOME}/.steam/root/compatibilitytools.d
+
+URL="https://github.com/${REPO}/releases/latest/download/${FILE}"
+DST_FILE="${DST_DIR}/${FILE}"
+
+curl -fLo ${DST_FILE} --create-dirs ${URL}
+
+tar -xf ${DST_FILE} -C ${DST_DIR}
+
+rm ${DST_FILE}
