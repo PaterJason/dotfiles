f718c3a59598126526c83da5ca9001599498994b
Jason Paterson <jasonbipaterson@gmail.com>
2021-11-08 13:45:20 +0000

    git email

--- stow/term/.config/fish/config.fish
+++ stow/term/.config/fish/config.fish
@@ -1,5 +1,5 @@
 set -gx NPM_PACKAGES "$HOME/.npm-packages"
-set -gx PATH $PATH $NPM_PACKAGES/bin $HOME/.cargo/bin
+set -gx PATH $PATH $NPM_PACKAGES/bin $HOME/.cargo/bin $HOME/.local/bin
 # set -gx MANPATH $NPM_PACKAGES/share/man $MANPATH
 
 set -g fish_greeting
