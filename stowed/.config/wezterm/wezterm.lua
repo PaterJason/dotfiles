d6b744de03e672518cdafed74e60db2b36b10f6e
Jason Paterson <jasonbipaterson@gmail.com>
2022-02-10 15:31:53 +0000

    Return to gnome

--- stowed/.config/wezterm/wezterm.lua
+++ stowed/.config/wezterm/wezterm.lua
@@ -1,24 +1,28 @@
 local wezterm = require 'wezterm'
-local HOME = os.getenv('HOME')
+local HOME = os.getenv 'HOME'
 
 return {
-  default_prog = {"/usr/bin/fish", "-l"},
+  default_prog = { '/usr/bin/tmux', '-l' },
 
-  color_scheme_dirs = { HOME .. '/.local/share/nvim/site/pack/packer/start/tokyonight.nvim/extras'},
+  color_scheme_dirs = { HOME .. '/.local/share/nvim/site/pack/packer/start/tokyonight.nvim/extras' },
   color_scheme = 'wezterm_tokyonight_night',
 
-  font = wezterm.font('Cascadia Code'),
+  font = wezterm.font 'Iosevka',
   font_size = 11.0,
   freetype_load_target = 'HorizontalLcd',
 
   hide_tab_bar_if_only_one_tab = true,
   window_frame = {
-    font = wezterm.font('Inter'),
+    font = wezterm.font 'Iosevka',
     font_size = 11.0,
-    active_titlebar_bg = "#15161E",
-    inactive_titlebar_bg = "#15161E",
-    -- inactive_tab_edge = "#570000",
+    active_titlebar_bg = '#15161E',
   },
 
   enable_wayland = true,
+
+  initial_rows = 43,
+  initial_cols = 132,
+
+  disable_default_key_bindings = true,
+  disable_default_mouse_bindings = true,
 }
