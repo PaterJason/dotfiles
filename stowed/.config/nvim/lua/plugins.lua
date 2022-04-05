59aca0455d9c8d315669fd39f5de814c66ce1623
Jason Paterson <jasonbipaterson@gmail.com>
2022-04-06 00:13:03 +0100

    Configure nvim-autopairs for lisp

--- stowed/.config/nvim/lua/plugins.lua
+++ stowed/.config/nvim/lua/plugins.lua
@@ -62,26 +62,31 @@ packer.startup {
       'windwp/nvim-autopairs',
       after = 'nvim-cmp',
       config = function()
-        require('nvim-autopairs').setup {
-          disable_filetype = {
-            'TelescopePrompt',
-            -- Parinfer filetypes
-            'clojure',
-            'scheme',
-            'lisp',
-            'racket',
-            'hy',
-            'fennel',
-            'janet',
-            'carp',
-            'wast',
-            'yuck',
-            'query',
-          },
+        local npairs = require 'nvim-autopairs'
+
+        npairs.setup {
           check_ts = true,
           enable_check_bracket_line = false,
         }
         require('cmp').event:on('confirm_done', require('nvim-autopairs.completion.cmp').on_confirm_done())
+
+        local lisp_filetypes = {
+          'clojure',
+          'scheme',
+          'lisp',
+          'racket',
+          'hy',
+          'fennel',
+          'janet',
+          'carp',
+          'wast',
+          'yuck',
+        }
+
+        vim.list_extend(npairs.get_rule("'")[1].not_filetypes, lisp_filetypes)
+        npairs.get_rule("(").not_filetypes = lisp_filetypes
+        npairs.get_rule("{").not_filetypes = lisp_filetypes
+        npairs.get_rule("[").not_filetypes = lisp_filetypes
       end,
     }
 
