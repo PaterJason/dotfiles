1c6f78d409e250ace8afc3bb0f74d71a0ac070cf
Jason Paterson <jason.paterson@agoradcm.com>
2022-11-24 16:59:20 +0000

    Fix neovide multigrid flag

--- stowed/.config/nvim/lua/plugins/dap.lua
+++ stowed/.config/nvim/lua/plugins/dap.lua
@@ -5,7 +5,15 @@ local dapui = require "dapui"
 dapui.setup {
   icons = { expanded = "-", collapsed = "+" },
 }
-dap.listeners.after.event_initialized["dapui_config"] = dapui.open
+dap.listeners.after.event_initialized["dapui_config"] = function()
+  dapui.open {}
+end
+dap.listeners.before.event_terminated["dapui_config"] = function()
+  dapui.close {}
+end
+dap.listeners.before.event_exited["dapui_config"] = function()
+  dapui.close {}
+end
 
 -- Configurations
 dap.configurations.lua = {
