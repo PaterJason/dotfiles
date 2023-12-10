3b993e54562430ce27cf5fff02b6897c3eb66878
Jason Paterson <jasonbipaterson@gmail.com>
2023-12-10 14:37:44 +0000

    Neovim: healthcheck like foldtext

--- stowed/.config/nvim/lua/jp/call.lua
+++ stowed/.config/nvim/lua/jp/call.lua
@@ -1,9 +1,32 @@
 local M = {}
 
 function M.foldtext()
+  if vim.opt_local.foldmethod:get() == "marker" then
+    return vim.fn.foldtext()
+  end
+
   local foldtext = vim.treesitter.foldtext()
   if type(foldtext) == "table" then
-    vim.list_extend(foldtext, { { "     ", {} }, { vim.v.foldend - vim.v.foldstart + 1 .. " lines", { "NonText" } } })
+    local is_diagnostic = vim
+      .iter(vim.diagnostic.get(0, {
+        severity = { min = vim.diagnostic.severity.WARN },
+      }))
+      :any(
+        ---@param diagnostic Diagnostic
+        function(diagnostic)
+          return diagnostic.lnum + 1 >= vim.v.foldstart and diagnostic.lnum + 1 <= vim.v.foldend
+        end
+      )
+
+    foldtext[#foldtext + 1] = {
+      " +"
+        .. (is_diagnostic and "WE" or "--")
+        .. string.sub(vim.v.folddashes, 2)
+        .. " "
+        .. vim.v.foldend - vim.v.foldstart + 1
+        .. " lines",
+      { "NonText" },
+    }
   end
   return foldtext
 end
