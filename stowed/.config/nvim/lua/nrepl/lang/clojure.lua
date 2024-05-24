35f4b62e0b8337b2b607387cea3c77044375a5fe
Jason Paterson <jasonbipaterson@gmail.com>
2024-05-24 13:44:25 +0100

    neovim nrepl commands

--- stowed/.config/nvim/lua/nrepl/lang/clojure.lua
+++ stowed/.config/nvim/lua/nrepl/lang/clojure.lua
@@ -19,15 +19,47 @@ vim.api.nvim_buf_create_user_command(0, "NreplOp", function(info)
 
   local fargs = info.fargs
   local op = fargs[1]
-  local args = vim.list_slice(fargs, 2)
 
-  require("nrepl.client").write(client, {
+  local data = {
     op = op,
-  })
+  }
+
+  local args = vim.list_slice(fargs, 2)
+  for _, arg in ipairs(args) do
+    local n = string.find(arg, "=", 2, true)
+    local key = string.sub(arg, 0, n - 1)
+    local value = string.sub(arg, n + 1)
+    if key ~= "" and value ~= "" then data[key] = value end
+  end
+
+  require("nrepl.client").write(client, data)
 end, {
-  range = true,
   nargs = "+",
   complete = function(arg_lead, cmd_line, cursor_pos)
-    return vim.tbl_keys(STATE.ops)
+    local _, arg_n = string.gsub(string.sub(cmd_line, 1, cursor_pos), " ", "")
+    if arg_n == 1 then
+      return vim
+        .iter(STATE.ops)
+        :map(function(key, value)
+          if string.sub(key, 1, string.len(arg_lead)) == arg_lead then return key end
+        end)
+        :totable()
+    else
+      local op = vim.split(cmd_line, " ", { plain = true })[2]
+
+      -- return vim
+      --   .iter(STATE.ops[op])
+      --   :map(function(key, value)
+      --     if string.sub(key, 1, string.len(arg_lead)) == arg_lead then return key end
+      --   end)
+      --   :totable()
+    end
   end,
+})
+
+vim.api.nvim_buf_create_user_command(0, "NreplLog", function(info)
+  local buf = require("nrepl.log").get_buf()
+  vim.cmd({ cmd = "buffer", args = { tostring(buf) } })
+end, {
+  nargs = 0,
 })
