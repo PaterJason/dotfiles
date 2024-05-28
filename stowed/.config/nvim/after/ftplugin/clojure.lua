64ac0c16ff70f6ced46f53548e4f785c5cd060aa
Jason Paterson <jasonbipaterson@gmail.com>
2024-05-28 10:30:16 +0100

    neovim: add cloljure nrepl init config

--- stowed/.config/nvim/after/ftplugin/clojure.lua
+++ stowed/.config/nvim/after/ftplugin/clojure.lua
@@ -2,4 +2,14 @@ vim.g.clojure_fuzzy_indent_patterns = { "^with", "^def", "^let", "^comment$" }
 vim.g.clojure_maxlines = 0
 vim.g.clojure_align_subforms = 1
 
--- require"nrepl.lang.clojure"
+if true then
+  require("nrepl").init()
+
+  local action = require("nrepl.action")
+  vim.keymap.set("n", "<LocalLeader>nf", action.load_file, { desc = "Load file" })
+  vim.keymap.set("n", "<LocalLeader>ne", action.eval_cursor, { desc = "Eval cursor symbol" })
+  vim.keymap.set("n", "<LocalLeader>np", action.eval_input, { desc = "Eval input" })
+  vim.keymap.set("n", "<LocalLeader>ni", action.interrupt, { desc = "Interrupt" })
+  vim.keymap.set("n", "<LocalLeader>nK", action.lookup_hover, { desc = "Hover lookup" })
+  vim.keymap.set("n", "<LocalLeader>nl", "<Cmd>vsplit | Nrepl log<CR>", { desc = "Hover lookup" })
+end
