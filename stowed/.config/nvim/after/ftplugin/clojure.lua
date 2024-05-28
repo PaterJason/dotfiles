dc05181c9e91ddd7837d99d49a68aefb0e7f3ccd
Jason Paterson <jasonbipaterson@gmail.com>
2024-05-28 23:42:25 +0100

    nvim: use buffer local nrepl mappings

--- stowed/.config/nvim/after/ftplugin/clojure.lua
+++ stowed/.config/nvim/after/ftplugin/clojure.lua
@@ -6,10 +6,15 @@ if true then
   require("nrepl").init()
 
   local action = require("nrepl.action")
-  vim.keymap.set("n", "<LocalLeader>nf", action.load_file, { desc = "Load file" })
-  vim.keymap.set("n", "<LocalLeader>ne", action.eval_cursor, { desc = "Eval cursor symbol" })
-  vim.keymap.set("n", "<LocalLeader>np", action.eval_input, { desc = "Eval input" })
-  vim.keymap.set("n", "<LocalLeader>ni", action.interrupt, { desc = "Interrupt" })
-  vim.keymap.set("n", "<LocalLeader>nK", action.lookup_hover, { desc = "Hover lookup" })
-  vim.keymap.set("n", "<LocalLeader>nl", "<Cmd>vsplit | Nrepl log<CR>", { desc = "Hover lookup" })
+  vim.keymap.set("n", "<LocalLeader>nf", action.load_file, { desc = "Load file", buffer = 0 })
+  vim.keymap.set("n", "<LocalLeader>ne", action.eval_cursor, { desc = "Eval cursor", buffer = 0 })
+  vim.keymap.set("n", "<LocalLeader>np", action.eval_input, { desc = "Eval input", buffer = 0 })
+  vim.keymap.set("n", "<LocalLeader>ni", action.interrupt, { desc = "Interrupt", buffer = 0 })
+  vim.keymap.set("n", "<LocalLeader>nh", action.lookup_hover, { desc = "Hover lookup", buffer = 0 })
+  vim.keymap.set(
+    "n",
+    "<LocalLeader>nl",
+    "<Cmd>vsplit | Nrepl log<CR>",
+    { desc = "Log vsplit", buffer = 0 }
+  )
 end
