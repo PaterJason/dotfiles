513994b62ed441406b55efdc8487e6ca112c43c3
Jason Paterson <jasonbipaterson@gmail.com>
2024-07-08 18:43:16 +0100

    Spin out nrepl config into plugin

--- stowed/.config/nvim/lua/plugins/cmp.lua
+++ stowed/.config/nvim/lua/plugins/cmp.lua
@@ -5,7 +5,6 @@ MiniDeps.later(function()
       "hrsh7th/cmp-nvim-lsp",
       "hrsh7th/cmp-nvim-lsp-signature-help",
       "rcarriga/cmp-dap",
-      "PaterJason/cmp-conjure",
     },
   })
 
--- stowed/.config/nvim/lua/plugins/misc_dev.lua
+++ stowed/.config/nvim/lua/plugins/misc_dev.lua
@@ -148,3 +148,67 @@ MiniDeps.later(function()
   end
   vim.keymap.set("i", "<C-l>", expand_snip_prefix, { desc = "Expand snippet" })
 end)
+
+MiniDeps.later(function()
+  MiniDeps.add({ source = "PaterJason/nvim-nrepl" })
+
+  local augroup = vim.api.nvim_create_augroup("nvim_nrepl", { clear = true })
+  vim.api.nvim_create_autocmd({ "FileType" }, {
+    group = augroup,
+    pattern = { "clojure" },
+    callback = function()
+      require("nrepl").init()
+
+      local action = require("nrepl.action")
+      vim.keymap.set(
+        { "n", "x" },
+        "<LocalLeader>e",
+        "<Plug>(NreplEvalOperator)",
+        { desc = "Eval", buffer = 0 }
+      )
+      vim.keymap.set(
+        "n",
+        "<LocalLeader>ee",
+        action.eval_cursor,
+        { desc = "Eval cursor", buffer = 0 }
+      )
+      vim.keymap.set("n", "<LocalLeader>lf", action.load_file, { desc = "Load file", buffer = 0 })
+      vim.keymap.set("n", "<LocalLeader>ll", "<Cmd>Nrepl log<CR>", { desc = "Log", buffer = 0 })
+      vim.keymap.set(
+        "n",
+        "<LocalLeader>ls",
+        function()
+          require("nrepl.prompt").open_win(true, {
+            win = -1,
+            vertical = false,
+          })
+        end,
+        {
+          desc = "Log split",
+          buffer = 0,
+        }
+      )
+      vim.keymap.set(
+        "n",
+        "<LocalLeader>lv",
+        function()
+          require("nrepl.prompt").open_win(true, {
+            win = -1,
+            vertical = true,
+          })
+        end,
+        { desc = "Log vsplit", buffer = 0 }
+      )
+      vim.keymap.set(
+        "n",
+        "<LocalLeader>lt",
+        "<Cmd>tabnew | Nrepl log<CR>",
+        { desc = "Log split", buffer = 0 }
+      )
+
+      vim.keymap.set("n", "<LocalLeader>K", action.hover, { desc = "Hover lookup", buffer = 0 })
+      vim.keymap.set("n", "<LocalLeader>np", action.eval_input, { desc = "Eval input", buffer = 0 })
+      vim.keymap.set("n", "<LocalLeader>ni", action.interrupt, { desc = "Interrupt", buffer = 0 })
+    end,
+  })
+end)
