be7ce581a3e1317ced0e89cc2b1387014764be0b
Jason Paterson <jasonbipaterson@gmail.com>
2025-01-30 09:49:30 +0000

    fix(neovim): appimage rename in download script

--- sh/curl_nvim_appimage.sh
+++ sh/curl_nvim_appimage.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
 pkill --echo --uid "$USER" --exact nvim
-# curl -L https://github.com/neovim/neovim/releases/latest/download/nvim.appimage --create-dirs -o ~/.local/bin/nvim
-curl --location https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage --create-dirs --output ~/.local/bin/nvim
+# curl -L https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.appimage --create-dirs -o ~/.local/bin/nvim
+curl --location https://github.com/neovim/neovim/releases/download/nightly/nvim-linux-x86_64.appimage --create-dirs --output ~/.local/bin/nvim
 chmod u+x ~/.local/bin/nvim
