33efb33b4a3c5dbbaa0f26d69937c40b762245f2
Jason Paterson <jasonbipaterson@gmail.com>
2025-02-26 02:04:55 +0000

    Refactor lspconfig root_dir config

--- stowed/.config/nvim/lua/plugins/lsp.lua
+++ stowed/.config/nvim/lua/plugins/lsp.lua
@@ -16,30 +16,25 @@ MiniDeps.now(function()
     if ok then
       ---@type vim.lsp.Config
       local lsp_config = vim.deepcopy(default_config, true)
-      lsp_config.root_dir = function(cb)
-        local bufnr = vim.api.nvim_get_current_buf()
-        local bufname = vim.api.nvim_buf_get_name(bufnr)
-        local get_root_dir = default_config.root_dir
-        local async = require("lspconfig.async")
-        async.run(function()
-          local root_dir
-          if type(get_root_dir) == "function" then
-            root_dir = get_root_dir(vim.fs.normalize(bufname), bufnr)
+      if type(default_config.root_dir) == "function" then
+        lsp_config.root_dir = function(cb)
+          local bufnr = vim.api.nvim_get_current_buf()
+          local bufname = vim.api.nvim_buf_get_name(bufnr)
+          local async = require("lspconfig.async")
+          async.run(function()
+            local root_dir = default_config.root_dir(vim.fs.normalize(bufname), bufnr)
             async.reenter()
             if not vim.api.nvim_buf_is_valid(bufnr) then return end
-          elseif type(get_root_dir) == "string" then
-            root_dir = get_root_dir
-          end
-          if root_dir then
-            cb(root_dir)
-          elseif default_config.single_file_support then
-            local pseudo_root = #bufname == 0 and vim.loop.cwd()
-              or vim.fs.dirname(vim.fs.normalize(bufname))
-            cb(pseudo_root)
-          end
-        end)
+            if root_dir then
+              cb(root_dir)
+            elseif default_config.single_file_support then
+              local pseudo_root = #bufname == 0 and vim.loop.cwd()
+                or vim.fs.dirname(vim.fs.normalize(bufname))
+              cb(pseudo_root)
+            end
+          end)
+        end
       end
-
       vim.lsp.config(server, lsp_config)
     end
   end
