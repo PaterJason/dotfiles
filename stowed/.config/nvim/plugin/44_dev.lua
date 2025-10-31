01b8f02bb9399eb9143bda88ffebd3faeed66e15
Jason Paterson <jasonbipaterson@gmail.com>
2025-10-31 02:00:52 +0000

    neovim, git: difftool

--- stowed/.config/nvim/plugin/44_dev.lua
+++ stowed/.config/nvim/plugin/44_dev.lua
@@ -2,6 +2,7 @@ vim.lsp.enable({
   'bashls', -- pacman -S bash-language-server
   'clojure_lsp', -- paru -S clojure-lsp-bin
   'cssls', -- pacman -S vscode-css-languageserver
+  'emmylua_ls', -- https://github.com/EmmyLuaLs/emmylua-analyzer-rust
   'eslint', -- pacman -S eslint-language-server
   'expert', -- https://github.com/elixir-lang/expert
   'fish_lsp', -- npm i -g fish-lsp
@@ -11,7 +12,6 @@ vim.lsp.enable({
   'jsonls', -- pacman -S vscode-json-languageserver
   'kotlin_lsp', -- https://github.com/Kotlin/kotlin-lsp
   'lemminx', -- paru -S lemminx
-  'emmylua_ls', -- https://github.com/EmmyLuaLs/emmylua-analyzer-rust
   'marksman', -- pacman -S marksman
   'rust_analyzer', -- pacman -S rust-analyzer
   'taplo', -- pacman -S taplo-cli
